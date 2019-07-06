fb0846bced8e54d882a913c11a847fd00bf9109b
dideex <wc34fun@mail.ru>
2019-07-06 16:23:17 +0800

    Reworked header and added storybook-knobs

--- src/components/header/header.stories.tsx
+++ src/components/header/header.stories.tsx
@@ -1,9 +1,29 @@
 import * as React from 'react'
 import { storiesOf } from '@storybook/react'
 import HeaderAppBar from './app-bar'
+import { withKnobs, text, select } from '@storybook/addon-knobs'
+import { THeaderTitles } from '../../routes'
 // import { action } from '@storybook/addon-actions'
 
-storiesOf('Header', module)
+const stories = storiesOf('Header menu', module)
+
+const label = 'Title'
+const options = {
+  News: 'News',
+  Signin: 'Sign in',
+  Profile: 'Profile',
+}
+const defaultValue = 'News'
+const groupId = 'GROUP-ID1'
+const titleSelect = select(label, options, defaultValue, groupId)
+
+// Add the `withKnobs` decorator to add knobs support to your stories.
+// You can also configure `withKnobs` as a global decorator.
+stories.addDecorator(withKnobs)
+
+stories
+  .add('Knobs header', () => (
+    <HeaderAppBar title={select(label, options, defaultValue, groupId) as THeaderTitles} />
+  ))
   .add('News header', () => <HeaderAppBar title="News" />, { info: { inline: true } })
   .add('Sign in header', () => <HeaderAppBar title="Sign in" />, { info: { inline: true } })
-  .add('Profile header', () => <HeaderAppBar title="Profile" />, { info: { inline: true } })
