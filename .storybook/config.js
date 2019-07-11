f4062d653d272362873fe5866b799e74ec3e7b5f
dideex <wc34fun@mail.ru>
2019-07-11 11:26:43 +0800

    Added viewport addon for storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,11 +1,13 @@
 import { configure, addDecorator, addParameters } from '@storybook/react'
 import { withInfo } from '@storybook/addon-info'
+import { INITIAL_VIEWPORTS as viewport } from '@storybook/addon-viewport'
 
 addParameters({
   options: {
     brandTitle: 'CRA TypeScript Kitchen Sink',
     brandUrl: 'https://github.com/storybooks/storybook/tree/master/examples/cra-ts-kitchen-sink',
   },
+  viewport,
 })
 addDecorator(withInfo())
 
--- src/components/header/header.stories.tsx
+++ src/components/header/header.stories.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import { storiesOf } from '@storybook/react'
 import HeaderAppBar from './app-bar'
-import { withKnobs, text, select } from '@storybook/addon-knobs'
+import { withKnobs, select } from '@storybook/addon-knobs'
 import { THeaderTitles } from '../../routes'
 // import { action } from '@storybook/addon-actions'
 
@@ -15,15 +15,16 @@ const options = {
 }
 const defaultValue = 'News'
 const groupId = 'GROUP-ID1'
-const titleSelect = select(label, options, defaultValue, groupId)
 
 // Add the `withKnobs` decorator to add knobs support to your stories.
 // You can also configure `withKnobs` as a global decorator.
 stories.addDecorator(withKnobs)
 
 stories
+  .addParameters({ viewport: { defaultViewport: 'iphone6' } })
   .add('Knobs header', () => (
     <HeaderAppBar title={select(label, options, defaultValue, groupId) as THeaderTitles} />
   ))
   .add('News header', () => <HeaderAppBar title="News" />, { info: { inline: true } })
   .add('Sign in header', () => <HeaderAppBar title="Sign in" />, { info: { inline: true } })
+  .add('IPhone', () => <HeaderAppBar title="Sign in" />, { viewport: { defaultViewport: 'iphonex' } })
